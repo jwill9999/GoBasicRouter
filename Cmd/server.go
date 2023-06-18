80f4ad38763182aeeb0596a9efe55506cf80b379
Jason Williams <jasonwilliams2308@gmail.com>
2023-06-18 18:14:50 +0100

    added logger

--- Cmd/server.go
+++ Cmd/server.go
@@ -1,13 +1,16 @@
 package cmd
 
 import (
-  "net/http"
-  "main/Router"
+	"log"
+	router "main/Router"
+	"net/http"
 )
 
 func ServerStart() {
-  r := &router.Router{}
-  router.RegisterRoutes(r);
-  http.ListenAndServe(":8000", r)
-  
+	const PORT = ":8000"
+	r := &router.Router{}
+	router.RegisterRoutes(r)
+	log.Printf("Server running on port %v", PORT)
+	http.ListenAndServe(PORT, r)
+
 }
--- Router/routes.go
+++ Router/routes.go
@@ -1,25 +1,21 @@
 package router
 
 import (
-  "net/http"
+	"net/http"
 )
 
 func RegisterRoutes(r *Router) {
-   
-    r.Route(http.MethodGet, "/", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("The Best Router!"))
-    })
 
-    r.Route(http.MethodGet, `/hello/(?P<Message>\w+)`, func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("Hello " + URLParam(r, "Message")))
-    })
+	r.Route(http.MethodGet, "/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("The server is now running in the browser!"))
+	})
 
-    r.Route(http.MethodGet, "/panic", func(w http.ResponseWriter, r *http.Request) {
-        panic("something bad happened!")
-    })
+	r.Route(http.MethodGet, `/hello/(?P<Message>\w+)`, func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello " + URLParam(r, "Message")))
+	})
 
-    
+	r.Route(http.MethodGet, "/panic", func(w http.ResponseWriter, r *http.Request) {
+		panic("something bad happened!")
+	})
 
-  }
-
-
+}
